c369a8b36076277bf08e1a3e91f2ba19a01e891d
EddieCodeHub <eddiemeekin@gmail.com>
2023-12-19 20:04:32 +0000

    add place holders to scores and eevnt listener for DOM load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// Wait for the DOM to finish loading before running the game
+//  Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+})
 
 
 function playerChoice() {
@@ -19,10 +25,12 @@ function displayWinner() {
 
 }
 
+
 function playerScoreUp() {
 
 }
 
+
 function cpuScoreUp() {
-    
+
 }
