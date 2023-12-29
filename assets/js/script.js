1e83a543fdc02d0f9a80d4dad00fcb501b606cb9
EddieCodeHub <eddiemeekin@gmail.com>
2023-12-29 14:31:05 +0000

    add number generator to generate the cpu choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,31 @@ var playerChoice;
 var cpuChoice;
 var result;
 
-
+// Takes players choice and creates variable
 function getPlayerChoice() {
 
 }
 
-
+// Randomly generates cpuChoice
 function getCpuChoice() {
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
 
+    if (randomNumber === 1) {
+        cpuChoice = "Rock";
+    }
+    if (randomNumber === 2) {
+        cpuChoice = "Paper";
+    }
+    if (randomNumber === 3) {
+        cpuChoice = "Scissors";
+    }
+    if (randomNumber === 4) {
+        cpuChoice = "Lizard";
+    }
+    if (randomNumber === 5) {
+        cpuChoice = "Spock";
+    }
+    
 }
 
 
