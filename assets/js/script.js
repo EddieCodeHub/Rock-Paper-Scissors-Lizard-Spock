9eb309dc5e4691ce3d1d73071d2a7cfecd8c2783
EddieCodeHub <eddiemeekin@gmail.com>
2023-12-31 13:51:27 +0000

    create functions to decide winner and increment scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,15 @@ var result;
 function getPlayerChoice(value) {
 
     playerChoice = value;
-    alert("you clicked button " + playerChoice);
     let choiceDisplay = document.getElementById("player-choice");
-    choiceDisplay.textContent = playerChoice;
+    choiceDisplay.innerHTML = playerChoice;
+    alert(playerChoice);
     getCpuChoice();
 }
 
 // Randomly generates cpuChoice
 function getCpuChoice() {
     var randomNumber = Math.floor(Math.random() * 5) + 1;
-
     if (randomNumber === 1) {
         cpuChoice = "Rock";
     }
@@ -37,27 +36,79 @@ function getCpuChoice() {
     if (randomNumber === 5) {
         cpuChoice = "Spock";
     }
-    
     document.getElementById("cpu-choice").innerHTML = cpuChoice
     decideWinner();
 }
 
-
+//decides winner and triggers appropriate function
 function decideWinner() {
 
+var outcomes = playerChoice + cpuChoice;
+
+switch(outcomes) {
+
+    // cases for player winning
+    case "RockScissors":
+    case "RockLizard":
+    case "PaperRock":
+    case "PaperSpock":
+    case "ScissorsPaper":
+    case "ScissorsLizard":
+    case "SpockRock":
+    case "SpockScissors":
+    case "LizardPaper":
+    case "LizardSpock":
+        result = "player-point";
+
+        break;
+    // cases for cpu winning
+    case "RockPaper":
+    case "RockSpock":
+    case "PaperScissors":
+    case "PaperLizard":
+    case "ScissorsRock":
+    case "ScissorSpock":
+    case "SpockPaper":
+    case "SpockLizard":
+    case "LizardRock":
+    case "LizardScissors":
+        result = "cpu-point";
+
+        break;
+    // cases for a draw 
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+    case "spockspock":
+    case "lizardlizard":
+        result = "draw";
+        break;
 }
 
-
-function displayWinner() {
+if (result === "player-point") {
+    playerScoreUp();
+}
+if (result === "cpu-point") {
+    cpuScoreUp();
+}
 
 }
 
-
+// increments player score
 function playerScoreUp() {
-
+    let playerScore = parseInt(document.getElementById("player-score").innerText);
+    document.getElementById("player-score").innerText = ++playerScore;
+    
 }
 
-
+// increments cpu score
 function cpuScoreUp() {
+    let cpuScore = parseInt(document.getElementById("cpu-score").innerText);
+    document.getElementById("cpu-score").innerText = ++cpuScore;
+    
+}
+
+// main game loop to decide overall winner and restart game
+function gameWinner() {
 
 }
